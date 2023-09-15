2131b79f131a8307d087ab6b86767dd127107f64
Turbotard <turbotard9@gmail.com>
2023-09-15 13:51:42 +0200

    fix register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,19 +6,21 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use function Webmozart\Assert\Tests\StaticAnalysis\email;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, ['required'=> true])
             ->add('firstname')
             ->add('lastname')
             ->add('birthday', DateType::class, array(
